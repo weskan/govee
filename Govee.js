ded0111d5dc79fd8f1f0569ef02859fcca8d4372
Wes <wes@kzws.co>
2026-03-10 18:00:04 -0400

    Update Govee.js

--- Govee.js
+++ Govee.js
@@ -908,16 +908,16 @@ const GoveeDeviceLibrary = {
 		{
 			name: "RGBIC TV Light Bars Pro Left",
 			ledCount: 10,
-			size: [10, 1],
+			size: [1, 10],
 			ledNames: ["Led 1", "Led 2", "Led 3", "Led 4", "Led 5", "Led 6", "Led 7", "Led 8", "Led 9", "Led 10"],
-			ledPositions: [[0, 0], [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0], [8, 0], [9, 0]],
+			ledPositions: [[0, 0], [0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7], [0, 8], [0, 9]],
 		},
 		{
 			name: "RGBIC TV Light Bars Pro Right",
 			ledCount: 10,
-			size: [10, 1],
+			size: [1, 10],
 			ledNames: ["Led 1", "Led 2", "Led 3", "Led 4", "Led 5", "Led 6", "Led 7", "Led 8", "Led 9", "Led 10"],
-			ledPositions: [[0, 0], [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0], [8, 0], [9, 0]],
+			ledPositions: [[0, 0], [0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [0, 7], [0, 8], [0, 9]],
 		},
 	]
 },
