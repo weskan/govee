68eee3e1d023ecc55a12844f3e11af571fc85e74
Wes <wes@kzws.co>
2026-03-10 16:57:27 -0400

    Update Govee.js

--- Govee.js
+++ Govee.js
@@ -897,31 +897,31 @@ const GoveeDeviceLibrary = {
 		ledCount: 15
 	},
 	H6048: {
-		name: "Gaming Light Bars Pro",
-		deviceImage: "https://assets.signalrgb.com/devices/brands/govee/wifi/h6048.png",
-		sku: "H6048",
-		state: 1,
-		supportRazer: true,
-		supportDreamView: true,
-		ledCount: 0,
-		usesSubDevices: true,
-		subdevices: [
-			{
-				name: "Gaming Light Bars Pro",
-				ledCount: 63,
-				size: [63, 1],
-				ledNames: ["Led 1", "Led 2", "Led 3", "Led 4", "Led 5", "Led 6", "Led 7", "Led 8", "Led 9", "Led 10", "Led 11", "Led 12", "Led 13", "Led 14", "Led 15", "Led 16", "Led 17", "Led 18", "Led 19", "Led 20", "Led 21", "Led 22", "Led 23", "Led 24", "Led 25", "Led 26", "Led 27", "Led 28", "Led 29", "Led 30", "Led 31", "Led 32", "Led 33", "Led 34", "Led 35", "Led 36", "Led 37", "Led 38", "Led 39", "Led 40", "Led 41", "Led 42", "Led 43", "Led 44", "Led 45", "Led 46", "Led 47", "Led 48", "Led 49", "Led 50", "Led 51", "Led 52", "Led 53", "Led 54", "Led 55", "Led 56", "Led 57", "Led 58", "Led 59", "Led 60", "Led 61", "Led 62", "Led 63"],
-				ledPositions: [[0, 0], [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0], [8, 0], [9, 0], [10, 0], [11, 0], [12, 0], [13, 0], [14, 0], [15, 0], [16, 0], [17, 0], [18, 0], [19, 0], [20, 0], [21, 0], [22, 0], [23, 0], [24, 0], [25, 0], [26, 0], [27, 0], [28, 0], [29, 0], [30, 0], [31, 0], [32, 0], [33, 0], [34, 0], [35, 0], [36, 0], [37, 0], [38, 0], [39, 0], [40, 0], [41, 0], [42, 0], [43, 0], [44, 0], [45, 0], [46, 0], [47, 0], [48, 0], [49, 0], [50, 0], [51, 0], [52, 0], [53, 0], [54, 0], [55, 0], [56, 0], [57, 0], [58, 0], [59, 0], [60, 0], [61, 0], [62, 0]],
-			},
-			{
-				name: "Gaming Light Bars Pro",
-				ledCount: 63,
-				size: [63, 1],
-				ledNames: ["Led 1", "Led 2", "Led 3", "Led 4", "Led 5", "Led 6", "Led 7", "Led 8", "Led 9", "Led 10", "Led 11", "Led 12", "Led 13", "Led 14", "Led 15", "Led 16", "Led 17", "Led 18", "Led 19", "Led 20", "Led 21", "Led 22", "Led 23", "Led 24", "Led 25", "Led 26", "Led 27", "Led 28", "Led 29", "Led 30", "Led 31", "Led 32", "Led 33", "Led 34", "Led 35", "Led 36", "Led 37", "Led 38", "Led 39", "Led 40", "Led 41", "Led 42", "Led 43", "Led 44", "Led 45", "Led 46", "Led 47", "Led 48", "Led 49", "Led 50", "Led 51", "Led 52", "Led 53", "Led 54", "Led 55", "Led 56", "Led 57", "Led 58", "Led 59", "Led 60", "Led 61", "Led 62", "Led 63"],
-				ledPositions: [[0, 0], [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0], [8, 0], [9, 0], [10, 0], [11, 0], [12, 0], [13, 0], [14, 0], [15, 0], [16, 0], [17, 0], [18, 0], [19, 0], [20, 0], [21, 0], [22, 0], [23, 0], [24, 0], [25, 0], [26, 0], [27, 0], [28, 0], [29, 0], [30, 0], [31, 0], [32, 0], [33, 0], [34, 0], [35, 0], [36, 0], [37, 0], [38, 0], [39, 0], [40, 0], [41, 0], [42, 0], [43, 0], [44, 0], [45, 0], [46, 0], [47, 0], [48, 0], [49, 0], [50, 0], [51, 0], [52, 0], [53, 0], [54, 0], [55, 0], [56, 0], [57, 0], [58, 0], [59, 0], [60, 0], [61, 0], [62, 0]],
-			},
-		]
-	},
+	name: "RGBIC TV Light Bars Pro",
+	deviceImage: "https://assets.signalrgb.com/devices/brands/govee/wifi/h6048.png",
+	sku: "H6048",
+	state: 1,
+	supportRazer: true,
+	supportDreamView: true,
+	ledCount: 0,
+	usesSubDevices: true,
+	subdevices: [
+		{
+			name: "RGBIC TV Light Bars Pro Left",
+			ledCount: 10,
+			size: [10, 1],
+			ledNames: ["Led 1", "Led 2", "Led 3", "Led 4", "Led 5", "Led 6", "Led 7", "Led 8", "Led 9", "Led 10"],
+			ledPositions: [[0, 0], [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0], [8, 0], [9, 0]],
+		},
+		{
+			name: "RGBIC TV Light Bars Pro Right",
+			ledCount: 10,
+			size: [10, 1],
+			ledNames: ["Led 1", "Led 2", "Led 3", "Led 4", "Led 5", "Led 6", "Led 7", "Led 8", "Led 9", "Led 10"],
+			ledPositions: [[0, 0], [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0], [8, 0], [9, 0]],
+		},
+	]
+},
 	H6051: {
 		name: "Table Lamp Lite",
 		deviceImage: "https://assets.signalrgb.com/devices/brands/govee/wifi/h6052.png",
